65056c20eaa8ce41d6392f3072cb46776d1f5123
Szymon Richert <szymon.rychu@gmail.com>
2018-01-17 07:37:54 +0100

    Bugfix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -527,7 +527,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {400/3,400/3,800,1700/3}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {400/3,400/3,800,1700/6}
 
 /**
  * Default Max Feed Rate (mm/s)
