9449a04047cce8e11035854bec775fcfcf4e64f4
Szymon Richert <szymon.rychu@gmail.com>
2018-01-18 22:16:53 +0100

    Update

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1068,10 +1068,10 @@
    stepperZ.stealthChop(1);\
    stepperX.sg_stall_value(-20);\
    stepperY.sg_stall_value(-20);\
-   stepperX.off_time(10);\
-   stepperY.off_time(10);\
-   stepperZ.off_time(10);\
-   stepperE0.off_time(10);\
+   stepperX.off_time(15);\
+   stepperY.off_time(15);\
+   stepperZ.off_time(15);\
+   stepperE0.off_time(15);\
   }
 
 #endif // HAVE_TMC2130
