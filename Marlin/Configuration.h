6229c4558f454ab68e51c2c8ddd5a3f5db9dc377
Szymon Richert <szymon.rychu@gmail.com>
2018-02-01 18:26:35 +0100

    Change x steps/mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -527,7 +527,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {400/3,400/3,800,675}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {200,400/3,800,675}
 
 /**
  * Default Max Feed Rate (mm/s)
