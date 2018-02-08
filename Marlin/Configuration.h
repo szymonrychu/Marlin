0f568a6122a6677a5c51d60f321d2f8e502aa623
Szymon Richert <szymon.rychu@gmail.com>
2018-02-08 22:08:06 +0100

    Bugfix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -676,7 +676,7 @@
 //  (0,0)
 #define X_PROBE_OFFSET_FROM_EXTRUDER 22  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 9  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER 1   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -1   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
