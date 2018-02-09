bcbe991d6e2174acd692c71580f01f5c5e8867f2
Szymon Richert <szymon.rychu@gmail.com>
2018-02-09 20:59:37 +0100

    Bugfix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -534,7 +534,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 150}
+#define DEFAULT_MAX_FEEDRATE          {500, 500, 200, 150}
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
